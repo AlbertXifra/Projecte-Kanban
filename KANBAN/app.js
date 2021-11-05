ed259fcbd47463471277bd84f43c592b0af4ddc3
amope02 <84399822+amope02@users.noreply.github.com>
2021-11-05 15:09:57 +0100

    Creant taula ToDo

--- KANBAN/app.js
+++ KANBAN/app.js
@@ -1,7 +1,8 @@
 // Porjcte SCRUM ALBEX
 
 //Declarar les variables
-let tasca = document.getElementById('tasca');
+let sel = document.getElementById('sel');
+let tasca = document.getElementsByClassName('tasca')[0];
 let codi = document.getElementById('codi');
 let descripcio = document.getElementById('descripcio');
 let dataInici = document.getElementById('dataInici');
@@ -9,28 +10,42 @@ let dataFinal = document.getElementById('dataFinal');
 let nom = document.getElementById('nom');
 let btn = document.getElementById('btn-guardar');
 
+
 mostrarDades();
 
 //Boto per entrar dades
 btn.addEventListener("click", () => {
     let tasquesAnteriors = JSON.parse(window.localStorage.getItem('tasca'));
+    console.log(window.localStorage.getItem('tasca'));
     if(tasquesAnteriors != undefined) {
         tasquesAnteriors.push({codi: codi.value, descripcio: descripcio.value, dataInici: dataInici.value, 
             dataFinal: dataFinal.value, nom: nom.value});
             window.localStorage.setItem('tasca', JSON.stringify(tasquesAnteriors));
-
     } else {
         let dades = {codi: codi.value, descripcio: descripcio.value, dataInici: dataInici.value, 
             dataFinal: dataFinal.value, nom: nom.value};
         window.localStorage.setItem('tasca', JSON.stringify([dades]));
     }
-    mostrarDades();
-    
+    //mostrarDades();
 });
+
 
 // Funcio mostrar dades
 function mostrarDades() {
     tasca.innerText='';
     let tascaAnterior = JSON.parse(window.localStorage.getItem('tasca'));
-    tascaAnterior.forEach((element) => tasca.innerText=tasca.innerText + JSON.stringify(element));
+    var count = 0;
+    if(tascaAnterior != undefined)
+    {
+        tascaAnterior.forEach((Object) =>JSON.stringify(Object))
+        {
+            let opt = document.createElement('option');
+            opt.value = codi.value;
+            opt.text = codi.value + ' ' + descripcio.value + ' ' + dataInici.value + ' ' + dataFinal.value + ' ' + nom.value;
+            sel.appendChild(opt);
+            count++;
+        }
+    }
+   
+    tasca.innerText = count.toString();
 }
