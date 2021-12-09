1af1d56426bd43154f24399a30783fba6fcc116e
amope02 <84399822+amope02@users.noreply.github.com>
2021-12-09 17:30:48 +0100

    Add files via upload
    guardar estat tasques

--- KANBAN/app.js
+++ KANBAN/app.js
@@ -4,9 +4,12 @@ let descripcio = document.getElementById('descripcio');
 let dataInici = document.getElementById('dataInici');
 let dataFinal = document.getElementById('dataFinal');
 let nom = document.getElementById('nom');
+let estat = document.getElementById('estat');
 let tria = document.getElementById('tria');
 let btn = document.getElementById('btn-Crear');
-let div1 = document.getElementsByClassName('div1')[0];
+let div1 = document.getElementById('div1');
+let div2 = document.getElementById('div2');
+let div3 = document.getElementById('div3');
 
 mostrarDades();
 
@@ -26,12 +29,12 @@ btn.addEventListener("click", () => {
                 //If per comprovar que el codi no sigui mes petit o igual a 0
                 if(tasquesAnteriors != undefined) {
                     tasquesAnteriors.push({codi: codi.value, descripcio: descripcio.value, dataInici: dataInici.value, 
-                    dataFinal: dataFinal.value, nom: nom.value, tria: tria.value});
+                    dataFinal: dataFinal.value, nom: nom.value, tria: tria.value, estat: estat.value});
                     window.localStorage.setItem('tasca', JSON.stringify(tasquesAnteriors));
                 }   
                 else {
                     let dades = {codi: codi.value, descripcio: descripcio.value, dataInici: dataInici.value, 
-                    dataFinal: dataFinal.value, nom: nom.value, tria: tria.value};
+                    dataFinal: dataFinal.value, nom: nom.value, tria: tria.value, estat: estat.value};
                     window.localStorage.setItem('tasca', JSON.stringify([dades]));
                 }
                 mostrarDades();
@@ -81,25 +84,66 @@ function negatiuCodi() {
 // Funcio mostrar dades a la taula
 function mostrarDades() {
     div1.innerText = '';
+    div2.innerText = '';
+    div3.innerText = '';
     let tascaAnterior = JSON.parse(window.localStorage.getItem('tasca'));
     if(tascaAnterior != undefined)
     {
         for(let i = 0; i < tascaAnterior.length; i++)
         {
-            if(tascaAnterior[i].tria == 'urgent')
+            if(tascaAnterior[i].estat == 'todo')
             {
-                div1.innerHTML += `<p id="${'drag' + tascaAnterior[i].codi}" draggable="true" ondragstart="drag(event)" class="urgent">
-                ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                if(tascaAnterior[i].tria == 'urgent')
+                {
+                    div1.innerHTML += `<p id="${i}" draggable="true" ondragstart="drag(event)" class="urgent">
+                    ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                }
+                else if(tascaAnterior[i].tria == 'poc urgent')
+                {
+                    div1.innerHTML += `<p id="${i}" draggable="true" ondragstart="drag(event)" class="poc">
+                    ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                }
+                else if(tascaAnterior[i].tria == 'molt urgent')
+                {
+                    div1.innerHTML += `<p id="${i}" draggable="true" ondragstart="drag(event)" class="molt">
+                    ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                }
             }
-            else if(tascaAnterior[i].tria == 'poc urgent')
+            else if(tascaAnterior[i].estat == 'doing')
             {
-                div1.innerHTML += `<p id="${'drag' + tascaAnterior[i].codi}" draggable="true" ondragstart="drag(event)" class="poc">
-                ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                if(tascaAnterior[i].tria == 'urgent')
+                {
+                    div2.innerHTML += `<p id="${i}" draggable="true" ondragstart="drag(event)" class="urgent">
+                    ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                }
+                else if(tascaAnterior[i].tria == 'poc urgent')
+                {
+                    div2.innerHTML += `<p id="${i}" draggable="true" ondragstart="drag(event)" class="poc">
+                    ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                }
+                else if(tascaAnterior[i].tria == 'molt urgent')
+                {
+                    div2.innerHTML += `<p id="${i}" draggable="true" ondragstart="drag(event)" class="molt">
+                    ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                }
             }
-            else if(tascaAnterior[i].tria == 'molt urgent')
+            else if(tascaAnterior[i].estat == 'done')
             {
-                div1.innerHTML += `<p id="${'drag' + tascaAnterior[i].codi}" draggable="true" ondragstart="drag(event)" class="molt">
-                ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                if(tascaAnterior[i].tria == 'urgent')
+                {
+                    div3.innerHTML += `<p id="${i}" draggable="true" ondragstart="drag(event)" class="urgent">
+                    ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                }
+                else if(tascaAnterior[i].tria == 'poc urgent')
+                {
+                    div3.innerHTML += `<p id="${i}" draggable="true" ondragstart="drag(event)" class="poc">
+                    ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                }
+                else if(tascaAnterior[i].tria == 'molt urgent')
+                {
+                    div3.innerHTML += `<p id="${i}" draggable="true" ondragstart="drag(event)" class="molt">
+                    ${tascaAnterior[i].codi + ' ' + tascaAnterior[i].descripcio + ' ' + tascaAnterior[i].dataInici + ' ' + tascaAnterior[i].dataFinal + ' ' + tascaAnterior[i].nom + '<button class="btn-delete" id = "delete-button" onclick="eliminar_tasques(' + i})">Delete</button></p>`
+                }
             }
         }
     }
@@ -108,19 +152,39 @@ function mostrarDades() {
 }
 
 // Funcio per fer el drag and drop a la taula
-function allowDrop(ev) {
+function allowDrop(ev){
     ev.preventDefault();
-  }
+}
   
-  function drag(ev) {
+function drag(ev){
     ev.dataTransfer.setData("text", ev.target.id);
-  }
+}
   
-  function drop(ev) {
+function drop(ev)
+{
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     console.log(data);
     ev.target.appendChild(document.getElementById(data));
+    console.log(ev.target.id);
+
+    let tasques = JSON.parse(window.localStorage.getItem('tasca'));
+
+    if(ev.target.id == "div3")
+    {
+        tasques[data] = {codi: tasques[data].codi, descripcio: tasques[data].descripcio, dataInici: tasques[data].dataInici, 
+        dataFinal: tasques[data].dataFinal, nom: tasques[data].nom, tria: tasques[data].tria, estat: "done"};
+    }
+    else if(ev.target.id == "div1") {
+        tasques[data] = {codi: tasques[data].codi, descripcio: tasques[data].descripcio, dataInici: tasques[data].dataInici, 
+        dataFinal: tasques[data].dataFinal, nom: tasques[data].nom, tria: tasques[data].tria, estat: "todo"};
+    }
+    else if(ev.target.id == "div2") {
+        tasques[data] = {codi: tasques[data].codi, descripcio: tasques[data].descripcio, dataInici: tasques[data].dataInici, 
+            dataFinal: tasques[data].dataFinal, nom: tasques[data].nom, tria: tasques[data].tria, estat: "doing"};
+    }
+
+    window.localStorage.setItem('tasca', JSON.stringify(tasques));
 }
 
 function eliminar_tasques(id) {
